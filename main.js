f75b68976acad3a975a977ea79b3df10c4b2dbf6
NoahTennthon <97657427+NoahTennthon@users.noreply.github.com>
2025-02-09 04:31:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ setInterval(() => {
 fetch("/nav.html")
     .then(response => response.text())
     .then(data => {
-        document.getElementById("navbar-container").innerHTML = data;
+        document.getElementById("navbar").innerHTML = data;
     })
     .catch(error => console.error("Error loading navbar:", error));
 
